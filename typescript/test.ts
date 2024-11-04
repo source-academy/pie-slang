eaf74258d7f0643e7de7e08dbae834e7a38d75eb
ZhangQixiang123 <e1264831@u.nus.edu>
2024-11-04 22:44:02 +0800

    Write tests
    Co-authored-by: Feng <dingf3ng@users.noreply.github.com>
    Co-authored-by: eddy <kaoxi998533@users.noreply.github.com>

---
+++ typescript/test.ts
@@ -0,0 +1,54 @@
+import {
+  Src, 
+  SrcStx,
+  initCtx,
+  ctxToEnv,
+  BindingSite,
+  MetaVar,
+  go,
+} from './basics';
+import {
+  valOf,
+  PIType,
+} from './normalize';
+import {} from './typechecker';
+
+import {
+  rep
+} from './rep';
+
+import { Location, Syntax } from './locations';
+const nl = new Location(new Syntax(), false);
+
+describe("valOf", () => {
+  it("should return ZERO", () => {
+    const result0 = valOf(ctxToEnv(initCtx), ['the', 'Nat', 'zero'])
+    expect(result0).toEqual('ZERO');
+  });
+});
+
+
+describe ("lambda(var) var", () => {
+  it("should return a pie expression", () => {
+    const result = rep(initCtx, new Src(nl, ['the', new Src(nl, ['->', new Src(nl, 'Nat'), new Src(nl, 'Nat'), []]), 
+  new Src(nl, ['λ', [new BindingSite(nl, Symbol('myVar'))], new Src(nl, Symbol('x'))])]));
+    const actual = new go(['the', ['Π', ['x', 'Nat'], 'Nat'], ['λ', 'myVar', 'myVar']]);
+    expect(result).toEqual(actual);
+  });
+
+  it("case lambda(x x) => x", () => {
+    const src = new Src(nl, ['the', 
+                            new Src(nl, ['->', new Src(nl, 'Nat'), new Src(nl, 'Nat'), [new Src(nl, 'Nat')]]), 
+                            new Src(nl, ['λ', [new BindingSite(nl, Symbol('x')), new BindingSite(nl, Symbol('x'))], 
+                                              new Src(nl, Symbol('x'))])]);
+    const actual = new go(['the', ['Π', ['x', 'Nat'], ['Π', ['x1', 'Nat'], 'Nat']], ['λ', 'x', ['λ', 'x1', 'x1']]]);
+    expect(rep(initCtx,src)).toEqual(actual);       
+  })
+  
+  it("case lambda(z) => z", () => {
+    const src = new Src(nl, ['the', new Src(nl, ['->', new Src(nl, 'Nat'), new Src(nl, 'Nat'), []]), new Src(nl, ['λ', [new BindingSite(nl, Symbol('z'))], new Src(nl, Symbol('z'))])]);
+    const actual = new go(['the', ['Π', ['z', 'Nat'], 'Nat', 'z']])
+    expect(rep(initCtx, src)).toEqual(actual);  
+  })
+});
+
