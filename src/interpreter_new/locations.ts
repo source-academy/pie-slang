1b89f80347879346db559700dd1a540e6db93f8c
ZhangQixiang123 <e1264831@u.nus.edu>
2025-02-25 18:00:50 +0800

    modify location

--- src/interpreter_new/locations.ts
+++ src/interpreter_new/locations.ts
@@ -1,41 +1,41 @@
 import { TokenType } from './tokenTypes';
 import { SourceLocation } from './types/utils';
+import { Location as Syntax } from '../scheme_parser/transpiler/types/location';
 
-class Position {
-  constructor(
-    public line: number,
-    public column: number
-  ) { }
-}
+// class Position {
+//   constructor(
+//     public line: number,
+//     public column: number
+//   ) { }
+// }
 
-class Token {
-  type: TokenType;
-  source: string;
-  literal: any;
-  pos: Position;
-  endPos: Position;
+// class Token {
+//   type: TokenType;
+//   source: string;
+//   literal: any;
+//   pos: Position;
+//   endPos: Position;
 
-  constructor(
-    type: TokenType,
-    source: any,
-    literal: any,
-    line: number,
-    col: number
-  ) {
-    this.type = type;
-    this.source = source;
-    this.literal = literal;
-    this.pos = new Position(line, col);
-    this.endPos = new Position(line, col + source.length - 1);
-  }
+//   constructor(
+//     type: TokenType,
+//     source: any,
+//     literal: any,
+//     line: number,
+//     col: number
+//   ) {
+//     this.type = type;
+//     this.source = source;
+//     this.literal = literal;
+//     this.pos = new Position(line, col);
+//     this.endPos = new Position(line, col + source.length - 1);
+//   }
 
-  public toString(): string {
-    return `${this.source}`;
-  }
-}
+//   public toString(): string {
+//     return `${this.source}`;
+//   }
+// }
 
 // Define Syntax as a node on AST
-type Syntax = Token;
 
 export class Location {
   constructor(
@@ -46,15 +46,10 @@ export class Location {
   public locationToSrcLoc(): SourceLocation {
     const stx = this.source;
     return new SourceLocation(
-      
+      this.source.start.line,
+      this.source.start.column,
+      this.source.end.line,
+      this.source.end.column
+    )
   }
-}
-
-export function locationToSrcLoc(loc: Location): [string | number, number, number] {
-  const stx = loc.source;
-  return [
-    stx.source, 
-    stx.pos.line,
-    stx.pos.column,
-  ];
 }
