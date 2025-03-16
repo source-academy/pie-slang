509fe11451a796c75e08b035e42004ee54cff95e
Li_Daoxin <lidaoxinldx@gmail.com>
2025-03-16 12:21:39 +0800

    update test

--- src/interpreter_new/__tests__/test.ts
+++ src/interpreter_new/__tests__/test.ts
@@ -1,84 +1,122 @@
-// import 'jest';
+import 'jest';
 
-// import {parsePie} from ''
+import {Parser} from '../parser';
+import * as util from 'util';
+import {go} from '../types/utils';
+import {initCtx} from '../types/contexts';
 
-// test("Transforming function type -> to Π", () => {
-//   const parsed = parsePie("(the (-> Nat Nat Nat) (lambda (x x) x))");
-//   expect(parsed).toEqual(
-//     ["the", ["Π", [["x", "Nat"]], ["Π", [["x₁", "Nat"]], "Nat"]], ["λ", ["x", "x"], "x"]]
-//   );
-// });
 
-// test("", () => {
-//   const parsed = parsePie('(the (-> Nat Nat) (λ (z) z))');
-//   expect(parsed).toEqual(
-//     ["the", ['Π', [["x", 'Nat']], 'Nat'],['λ', ["z"], "z"]]
-//   )
-// });
+const parser = new Parser();
 
-// test("Evaluating which-Nat expression", () => {
-//   const parsed = parsePie("(which-Nat 1 2 (lambda (x) x))");
-//   expect(parsed).toEqual(["the", "Nat", "zero"]);
-// });
+describe("demo", () => {
 
-// test("", () => {
-//   const parsed = parsePie("(which-Nat 0 2 (lambda (x) x))");
-//   expect(parsed).toEqual(["the", "Nat", ["add1", ["add1", "zero"]]])
-// });
+  it("Pie demo", () => {
+    const src = parser.parsePie('(-> Nat Nat Nat Nat Nat)');
+    const actual = new go(['the', 'U', ['Π', [[Symbol('x'), 'Nat']], 
+                                         ['Π', [[Symbol('x₁'), 'Nat']], 
+                                           ['Π', [[Symbol('x₂'), 'Nat']], 
+                                             ['Π', [[Symbol('x₃'), 'Nat']], 
+                                                  'Nat']]]]]);
+    console.log('result', util.inspect(rep(initCtx, src), false, null, true));
+    expect(rep(initCtx, src)).toEqualWithSymbols(actual);
+                                                  
+    });
 
-// test("Transforming higher-order function type", () => {
-//   const parsed = parsePie("(the (-> (-> Nat Nat) Nat Nat) (lambda (f x) (f x)))");
-//   expect(parsed).toEqual(
-//     ["the", 
-//       ["Π", [["x", ["Π", [["x", "Nat"]], "Nat"]]], ["Π", [["x₁", "Nat"]], "Nat"]], 
-//       ["λ", ["f"], ["λ", ["x"], ["f", "x"]]]
-//     ]
-//   );
-// });
+  it("Sigma demo", () => {
+    const src = parser.parsePie(
+                        `(the (-> Trivial
+                                         (Pair Trivial Trivial))
+                                     (lambda (x)
+                                       (cons x x)))`
+                                       );
+    const actual = new go(
+        ['the', ['Π', [[Symbol('x'), 'Trivial']], 
+        ['Σ', [[Symbol('x₁'), 'Trivial']], 'Trivial']], 
+        ['λ', [Symbol('x')],['cons', Symbol('sole'), Symbol('sole')]]]);
+    console.log('wcnm', util.inspect(rep(initCtx, src), false, null, true));
+    expect(rep(initCtx, src)).toEqualWithSymbols(actual);
+  });
 
-// test("Evaluating higher-order function with `which-Nat`", () => {
-//   const parsed = parsePie(`
-//     (the (-> Nat (-> Nat Nat) Nat)
-//          (lambda (x f)
-//            (which-Nat 2 x f)))
-//   `);
-//   expect(parsed).toEqual(
-//     [
-//       "the",
-//       [
-//         "Π", 
-//         [["x", "Nat"]],
-//         [
-//           "Π", 
-//           [["x₁", ["Π", [["x₁", "Nat"]], "Nat"]]], 
-//           "Nat"
-//         ]
-//       ],
-//       ["λ", ["x"], ["λ", ["f"], ["f", ["add1", "zero"]]]]
-//     ]
-//   );
-// });
+});
 
-// test("", () => {
-//   const parsed = parsePie(`
-//     (the (-> Nat (-> Nat Nat) Nat)
-//          (lambda (x f)
-//            (which-Nat x (add1 (add1 zero)) f)))
-//   `);
-//   expect(parsed).toEqual(
-//     [
-//       "the",
-//       [
-//         "Π", 
-//         [["x", "Nat"]],
-//         [
-//           "Π", 
-//           [["x₁", ["Π", [["x₁", "Nat"]], "Nat"]]], 
-//           "Nat"
-//         ]
-//       ],
-//       ["λ", ["x"], ["λ", ["f"], ["which-Nat", "x", ["the", "Nat", ["add1", ["add1", "zero"]]] , ["λ", ["n"], ["f", "n"]]]]]
-//     ]
-//   );
-// });
+test("Transforming function type -> to Π", () => {
+  const parsed = parser.parsePie("(the (-> Nat Nat Nat) (lambda (x x) x))");
+  const actual = new go(["the", ["Π", [["x", "Nat"]], ["Π", [["x₁", "Nat"]], "Nat"]], ["λ", ["x", "x"], "x"]])
+  expect(parsed).toEqual(
+    
+  );
+});
+
+test("", () => {
+  const parsed = parser.parsePie('(the (-> Nat Nat) (λ (z) z))');
+  expect(parsed).toEqual(
+    ["the", ['Π', [["x", 'Nat']], 'Nat'],['λ', ["z"], "z"]]
+  )
+});
+
+test("Evaluating which-Nat expression", () => {
+  const parsed = parser.parsePie("(which-Nat 1 2 (lambda (x) x))");
+  expect(parsed).toEqual(["the", "Nat", "zero"]);
+});
+
+test("", () => {
+  const parsed = parser.parsePie("(which-Nat 0 2 (lambda (x) x))");
+  expect(parsed).toEqual(["the", "Nat", ["add1", ["add1", "zero"]]])
+});
+
+test("Transforming higher-order function type", () => {
+  const parsed = parser.parsePie("(the (-> (-> Nat Nat) Nat Nat) (lambda (f x) (f x)))");
+  expect(parsed).toEqual(
+    ["the", 
+      ["Π", [["x", ["Π", [["x", "Nat"]], "Nat"]]], ["Π", [["x₁", "Nat"]], "Nat"]], 
+      ["λ", ["f"], ["λ", ["x"], ["f", "x"]]]
+    ]
+  );
+});
+
+test("Evaluating higher-order function with `which-Nat`", () => {
+  const parsed = parser.parsePie(`
+    (the (-> Nat (-> Nat Nat) Nat)
+         (lambda (x f)
+           (which-Nat 2 x f)))
+  `);
+  expect(parsed).toEqual(
+    [
+      "the",
+      [
+        "Π", 
+        [["x", "Nat"]],
+        [
+          "Π", 
+          [["x₁", ["Π", [["x₁", "Nat"]], "Nat"]]], 
+          "Nat"
+        ]
+      ],
+      ["λ", ["x"], ["λ", ["f"], ["f", ["add1", "zero"]]]]
+    ]
+  );
+});
+
+test("", () => {
+  const parsed = parser.parsePie(`
+    (the (-> Nat (-> Nat Nat) Nat)
+         (lambda (x f)
+           (which-Nat x (add1 (add1 zero)) f)))
+  `);
+  expect(parsed).toEqual(
+    [
+      "the",
+      [
+        "Π", 
+        [["x", "Nat"]],
+        [
+          "Π", 
+          [["x₁", ["Π", [["x₁", "Nat"]], "Nat"]]], 
+          "Nat"
+        ]
+      ],
+      ["λ", ["x"], ["λ", ["f"], ["which-Nat", "x", ["the", "Nat", ["add1", ["add1", "zero"]]] , ["λ", ["n"], ["f", "n"]]]]]
+    ]
+  );
+});
 
