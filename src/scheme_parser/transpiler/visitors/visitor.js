03d662e493d4b07a5bda2b9468e442f39ce51991
ZhangQixiang123 <e1264831@u.nus.edu>
2025-09-03 16:21:34 +0800

    clean up

--- src/scheme_parser/transpiler/visitors/visitor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Visitor interface for the AST.
- * Allows us to traverse the AST and perform operations on it.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
