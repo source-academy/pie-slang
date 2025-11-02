107538d49dfff02919c81333c011d07238c12494
Zhang Qixiang <e1264831@u.nus.edu>
2025-11-03 00:17:51 +0800

    fix export error (#87)
    * inductive type definition: core and value implemented
    
    * inductive datatype constructor generation(temp)
    
    * generate eliminator (hasve bugs)
    
    * in prograss
    
    * reuse PI
    
    * inductive type without parser
    
    * add parser and integration
    
    * migrate into independent test case
    
    * minimal setup
    
    * specifications
    
    * init
    
    * small fix
    
    * fix parameters
    
    * fix indices
    
    * eliminate inspects
    
    * small update
    
    * fix dotenv.config()
    
    * solve export error

--- src/pie_interpreter/try_llm/todo_solver.ts
+++ src/pie_interpreter/try_llm/todo_solver.ts
@@ -8,11 +8,7 @@ import { Location } from "../utils/locations";
 import { Context, readBackContext } from "../utils/context";
 import { Value } from "../types/value";
 import { Renaming } from "../typechecker/utils";
-import { config } from "dotenv";
-
-// Load environment variables from .env file
-config();
-
+import 'dotenv/config';
 // Get API key from environment variable
 const apiKey = process.env.GOOGLE_API_KEY;
 if (!apiKey) {
