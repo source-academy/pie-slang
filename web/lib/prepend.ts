c1ae28e8a57ce22e52d1bbebae386b3f67884a76
timthedev07 <timpersonal07@gmail.com>
2026-01-24 16:53:06 +0800

    add more defs

--- web/lib/prepend.ts
+++ web/lib/prepend.ts
@@ -383,5 +383,43 @@ Nat))
 (λ (n)
 (iter-Nat n
 1
-(+ 1))))`,
+(+ 1))))
+(claim +1=add1
+(Π ((n Nat))
+( Nat ( 1 n) (add1 n))))
+(define +1=add1
+(λ (n)
+(same (add1 n))))
+(claim base-incr=add1
+(= Nat (incr zero) (add1 zero)))
+(define base-incr=add1
+(same (add1 zero)))
+(claim mot-incr=add1
+(→ Nat
+U ))
+(define mot-incr=add1
+(λ (k)
+(= Nat (incr k) (add1 k))))
+(claim step-incr=add1
+(Π ((n-1 Nat))
+(→ (= Nat
+(incr n-1)
+(add1 n-1))
+(= Nat
+(add1
+(incr n-1))
+(add1
+(add1 n-1))))))
+(define step-incr=add1
+(λ (n-1)
+(λ (incr=add1n-1)
+(cong
+    incr=add1n-1 (+ 1)))))
+(define incr=add1
+(λ (n)
+(ind-Nat n
+mot-incr=add1
+base-incr=add1
+step-incr=add1)))
+`,
 ];
