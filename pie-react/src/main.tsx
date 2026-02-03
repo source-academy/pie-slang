e575c6078221c9309e57f2380a348500101e603b
Marcus <mingyang.ma@u.nus.edu>
2026-02-03 13:14:13 +0800

    fix(ci): Remove potentially missing index.css import

--- pie-react/src/main.tsx
+++ pie-react/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+// import './index.css'
 import { Provider } from 'react-redux'
 import { mainStore } from './store'
 import '@blueprintjs/core/lib/css/blueprint.css';
