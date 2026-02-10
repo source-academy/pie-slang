d1c48d56eee43334b04920ac675e9ffc963c5759
Marcus <mingyang.ma@u.nus.edu>
2026-02-10 10:38:33 +0800

    fix(ci): simplify todo-solver alias for Vite build

--- pie-react/vite.config.ts
+++ pie-react/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, type Plugin } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import path from 'path'
@@ -15,46 +15,10 @@ const polyfillShim = (name: string) =>
     `node_modules/vite-plugin-node-polyfills/shims/${name}/dist/index.js`
   );
 
-function resolveTodoSolverForBrowser(): Plugin {
-  return {
-    name: 'resolve-todo-solver-browser',
-    enforce: 'pre',
-    resolveId(source: string) {
-      if (
-        source === '../solver/todo-solver' ||
-        source === '../solver/todo-solver.ts' ||
-        source.endsWith('/solver/todo-solver') ||
-        source.endsWith('/solver/todo-solver.ts')
-      ) {
-        return todoSolverBrowserPath;
-      }
-      return null;
-    }
-  };
-}
-
-function rewriteTodoSolverImport(): Plugin {
-  return {
-    name: 'rewrite-todo-solver-import',
-    enforce: 'pre',
-    transform(code: string, id: string) {
-      if (id.endsWith('/src/pie-interpreter/typechecker/utils.ts')) {
-        return code.replace(
-          '../solver/todo-solver',
-          '../solver/todo-solver.browser'
-        );
-      }
-      return null;
-    }
-  };
-}
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    rewriteTodoSolverImport(),
-    resolveTodoSolverForBrowser(),
     nodePolyfills({
       exclude: ['fs'],
       globals: {
@@ -72,11 +36,9 @@ export default defineConfig({
       { find: 'vite-plugin-node-polyfills/shims/buffer', replacement: polyfillShim('buffer') },
       { find: 'vite-plugin-node-polyfills/shims/global', replacement: polyfillShim('global') },
       { find: 'vite-plugin-node-polyfills/shims/process', replacement: polyfillShim('process') },
-      // Force swap todo-solver to browser version
-      {
-        find: /\/todo-solver(\.ts)?$/,
-        replacement: todoSolverBrowserPath
-      }
+      // Force swap todo-solver to browser version (for the React build)
+      { find: '../solver/todo-solver', replacement: todoSolverBrowserPath },
+      { find: '../solver/todo-solver.ts', replacement: todoSolverBrowserPath }
     ]
   },
   server: {
